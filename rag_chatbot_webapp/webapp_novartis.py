94cd16ea47ffc779dc00b436b61cb5a74a5f57cc
Metin Turgal <metinturgal@gmail.com>
2024-05-03 11:11:31 +0300

    changed the main function to build the knowledge base from pdf files

--- rag_chatbot_webapp/webapp_novartis.py
+++ rag_chatbot_webapp/webapp_novartis.py
@@ -11,33 +11,22 @@ import numpy as np
 import time
 from langchain_openai import ChatOpenAI
 import config
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'this_is_bad_secret_key'
 
-# get your openai api key at https://platform.openai.com/
 OPENAI_API_KEY = config.OPENAI_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
-# get your cohere api key at https://cohere.com/
-COHERE_KEY = config.COHERE_KEY
-co = cohere.Client(COHERE_KEY)
 
 limit_input_tokens=4096
-file_path = 'data/dataset_folder/dataset_novartis'
+pdf_folder_path = 'data/dataset_folder/pdf_folder'
 prompt_template_path = 'data/prompt_template/prompt_template.txt'
 num_chunks_of_text = 8 # number of chunks of text to search via embeddings similarity
 
-
-def read_dataset():
-    # read file
-    with open(file_path, 'r', encoding="utf-8") as f:
-        document = f.read()
-    document_chunks = document.split('########')
-    return document_chunks
-
-document_chunks = read_dataset()
-print('doc_chunks len: ', len(document_chunks))
 
 def read_template():
     # read file
@@ -48,49 +37,22 @@ def read_template():
 prompt_template = read_template()
 
 
-#def get_context_embeddings_co():
-#    context_emb = co.embed(texts=document_chunks, input_type="search_document", model="embed-multilingual-v3.0").embeddings
-#    context_emb = np.asarray(context_emb)
-#    return context_emb
+loader = PyPDFDirectoryLoader(pdf_folder_path)
+docs = loader.load()
+faiss_index = FAISS.from_documents(docs, OpenAIEmbeddings(model='text-embedding-ada-002'))
 
-def get_context_embeddings_co(docs):
-    res = openai.embeddings.create(input=docs, model="text-embedding-ada-002")
-    doc_embeds = [r.embedding for r in res.data]
-    return np.asarray(doc_embeds)
-
-context_emb = get_context_embeddings_co(document_chunks[:1000])
-
-def generate_full_llm_query(query, document_chunks, prompt_template, limit_input_tokens=4096):
-
-    # cohere embeddings
-    #query_emb = co.embed(texts=[query], input_type="search_query", model="embed-multilingual-v3.0").embeddings
-    #openai embeddings
-    query_emb = openai.embeddings.create(input=query, model="text-embedding-ada-002")
-    query_emb = np.asarray(query_emb.data[0].embedding)
-
-    #Compute the dot product between query embedding and document embedding
-    scores = np.dot(query_emb, context_emb.T).squeeze()
-
-    max_idx = np.argsort(-scores)
-
-    context_chunks_initial = []
-    context_scores = []
-    for idx in max_idx[:num_chunks_of_text]:
-      context_scores.append(scores[idx])
-      context_chunks_initial.append(document_chunks[idx])
-    context_chunks = context_chunks_initial
-
-
+def generate_full_llm_query_faiss(query, prompt_template, limit_input_tokens=4096):
+    top_results = faiss_index.similarity_search(query, k= num_chunks_of_text)
     llm_full_query = ''
 
     # More complex logic is implemented to ensure the full llm query does not exceeds input token limit
     # i.e. finall llm query is cooked with size < limit_input_tokens
     correction_num_of_tokens = 500 # additionally decrease num of tokens by this number
     # Truncating chunks to the size of limit_input_tokens
-    num_of_chunks = len(context_chunks)
-    for iter in range(num_of_chunks):
-        print('context_chunks len:', len(context_chunks))
-        context_chunks_as_str = '\n###\n'.join([str(elem) for elem in context_chunks])
+    num_of_chunks = len(top_results)
+    for _ in range(num_of_chunks):
+        print('top results len:', len(top_results))
+        context_chunks_as_str = '\n###\n'.join([str(elem.page_content) for elem in top_results])
         llm_full_query = prompt_template.format(context=context_chunks_as_str, question=query)
         encoding = tiktoken.get_encoding("cl100k_base")
         num_tokens_template = len(encoding.encode(prompt_template))
@@ -98,20 +60,22 @@ def generate_full_llm_query(query, document_chunks, prompt_template, limit_input
         print('num_tokens', num_tokens)
 
         if num_tokens <= limit_input_tokens:
-          llm_full_query, num_tokens
-        elif len(context_chunks) == 1 and num_tokens > limit_input_tokens - correction_num_of_tokens:
+          return llm_full_query, num_tokens
+        elif len(top_results) == 1 and num_tokens > limit_input_tokens - correction_num_of_tokens:
           chunk_appendix = '\nDetails in the link:'
-          extracted_link = re.search(r'https://.+', context_chunks[0][-100:]).group(0)
+          # extracted_link = re.search(r'https://.+', context_chunks[0][-100:]).group(0)
+          extracted_link = top_results[0].metadata["source"]
           chunk_appendix = chunk_appendix + ' ' + extracted_link
           num_of_chars_to_cut = num_tokens - limit_input_tokens + num_tokens_template + correction_num_of_tokens
-          context_chunks[0] = context_chunks[0][:-num_of_chars_to_cut]
-          context_chunks[0] = context_chunks[0] + chunk_appendix
-          context_chunks_as_str = '\n###\n'.join([str(elem) for elem in context_chunks])
+          top_results[0].page_content = top_results[0].page_content[:-num_of_chars_to_cut]
+          top_results[0].page_content = top_results[0].page_content + chunk_appendix
+          print(top_results[0].page_content)
+          context_chunks_as_str = '\n###\n'.join([str(elem.page_content) for elem in top_results])
           llm_full_query = prompt_template.format(context=context_chunks_as_str, question=query)
           num_tokens = len(encoding.encode(llm_full_query))
-          llm_full_query, num_tokens
+          return llm_full_query, num_tokens
         elif num_tokens > limit_input_tokens - correction_num_of_tokens:
-          context_chunks = context_chunks[:-1]
+          top_results = top_results[:-1]
 
     return llm_full_query, context_chunks
 
@@ -130,7 +94,7 @@ def send_message():
 
     # Process user message here if needed
     now = datetime.datetime.now()
-    llm_full_query, context_chunks = generate_full_llm_query(query, document_chunks, prompt_template, limit_input_tokens=limit_input_tokens)
+    llm_full_query, context_chunks = generate_full_llm_query_faiss(query, prompt_template, limit_input_tokens=limit_input_tokens)
     # print('current full query:', llm_full_query)
     llm_answer = llm.invoke(llm_full_query)
     #llm_answer = llm_full_query  # this is a stub if llm initialization disabled
